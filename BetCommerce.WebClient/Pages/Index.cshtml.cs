3ddd9b478bd0dc4096ff16a992ad3ff1fb8d826f
George Wainaina Njeri <georgewainaina18@gmail.com>
2022-07-27 16:02:16 +0300

    adding basic ui template

---
+++ BetCommerce.WebClient/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BetCommerce.WebClient.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Redirect("/Shop");
+        }
+    }
+}
